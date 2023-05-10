c84cb1fcc3b0df1248d635aafc8b15f2cbf236b9
unknown <nicoamaciel@outlook.com>
2023-05-10 02:01:54 -0300

    02:01 10/05

--- negocio/ElementoNegocio.cs
+++ negocio/ElementoNegocio.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using dominio;
+using negocio;
+
 
 
 namespace ConexionDBAPOKEDEX
@@ -12,7 +14,38 @@ namespace ConexionDBAPOKEDEX
     {
         public List<Elementos> listar()
         {
-            return new List<Elementos>();
+
+            List<Elementos> lista = new List<Elementos>();
+            AccesoDatos datos = new AccesoDatos();
+
+
+            try
+            {
+                datos.setearConsulta("SELECT Id, Descripcion from ELEMENTOS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Elementos aux = new Elementos();
+                    aux.ID = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
         }
 
 
